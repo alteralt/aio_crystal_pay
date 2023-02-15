1cfea2e5a5348434920efbba98215db29609ddf0
alteralt <109542778+alteralt@users.noreply.github.com>
2023-02-15 13:47:14 +0300

    Add Programming Language :: Python :: 3.11 to setup.py

--- setup.py
+++ setup.py
@@ -39,6 +39,7 @@ setuptools.setup(
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
     ],
     project_urls={"Source": "https://github.com/{}/{}".format(author, name)},
 )
